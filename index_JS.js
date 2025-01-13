2f924dde8bd2c200420184071278c869a4612932
carotte100 <carottelenullos@gmail.com>
2025-01-13 14:22:21 +0100

    ajout option se souvenir
    sauvegarde dans la mémoire local si on clique sur le bouton

--- index_JS.js
+++ index_JS.js
@@ -4,23 +4,8 @@
 - mdp
 (boolean admin a revoir)
 */
-const submitBtn = document.getElementById("submit-btn");
-if (submitBtn) {
-    submitBtn.addEventListener("click", function(event) {
-        event.preventDefault();
-        
-        const nom = document.getElementById("nom").value;
-        const mail = document.getElementById("mail").value;
-        const motDePasse = document.getElementById("psw").value;
-        const seSouvenir = document.getElementById("remember").checked;
 
-        console.log("Nom :", nom);
-        console.log("Mail :", mail);
-        console.log("Mot de passe :", motDePasse);
-        console.log("Se souvenir de moi :", seSouvenir);
-    });
-}
-
+// pour la page mdp_oublié
 const mailBtn = document.getElementById("mail-btn");
 if (mailBtn) {
     mailBtn.addEventListener("click", function(event) {
@@ -29,4 +14,44 @@ if (mailBtn) {
         const mailRecup = document.getElementById("mail-recup").value;
         console.log("Mail pour réinitialisation :", mailRecup);
     });
-}
+};
+
+// sauvegarde local si se souvenir est coché
+document.addEventListener("DOMContentLoaded", function () {
+    const savedName = localStorage.getItem("nom");
+    const savedMail = localStorage.getItem("mail");
+    const savedsouvenir = localStorage.getItem("souvenir");
+
+    if (savedsouvenir === "true") {
+        document.getElementById("nom").value = savedName || "";
+        document.getElementById("mail").value = savedMail || "";
+        document.getElementById("souvenir").checked = true;
+    }
+
+    // Gérer le clic sur le bouton de connexion
+    document.getElementById("submit-btn").addEventListener("click", function (event) {
+        event.preventDefault();
+
+        const nom = document.getElementById("nom").value;
+        const mail = document.getElementById("mail").value;
+        const souvenir = document.getElementById("souvenir").checked;
+
+        if (souvenir) {
+            //sauvegarde dans la mémoire
+            localStorage.setItem("nom", nom);
+            localStorage.setItem("mail", mail);
+            localStorage.setItem("souvenir", true);
+        } else {
+            //supprime de la mémoire si on décoche
+            localStorage.removeItem("nom");
+            localStorage.removeItem("mail");
+            localStorage.setItem("souvenir", false);
+        }
+
+        // test dans la console
+        console.log("Nom :", nom);
+        console.log("Mail :", mail);
+        console.log("Se souvenir de moi :", souvenir);
+    });
+});
+
