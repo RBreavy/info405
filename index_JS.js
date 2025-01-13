140fd09088bc01f259f2e8eaadc4f9f802b77d82
carotte100 <carottelenullos@gmail.com>
2025-01-13 13:48:07 +0100

    changement mdp_oublie et js
    recuperation de la page mdp_oublier via js

--- index_JS.js
+++ index_JS.js
@@ -2,20 +2,31 @@
 - nom
 - mail
 - mdp
+(boolean admin a revoir)
 */
+const submitBtn = document.getElementById("submit-btn");
+if (submitBtn) {
+    submitBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+        
+        const nom = document.getElementById("nom").value;
+        const mail = document.getElementById("mail").value;
+        const motDePasse = document.getElementById("psw").value;
+        const seSouvenir = document.getElementById("remember").checked;
 
-document.getElementById("submit-btn").addEventListener("click", function(event) {
+        console.log("Nom :", nom);
+        console.log("Mail :", mail);
+        console.log("Mot de passe :", motDePasse);
+        console.log("Se souvenir de moi :", seSouvenir);
+    });
+}
 
-    event.preventDefault();
-    
-    const nom = document.getElementById("nom").value;
-    const mail = document.getElementById("mail").value;
-    const motDePasse = document.getElementById("psw").value;
-    const seSouvenir = document.getElementById("remember").checked;
-
-    // test affichage dans la console
-    console.log("Nom :", nom);
-    console.log("Mail :", mail);
-    console.log("Mot de passe :", motDePasse);
-    console.log("Se souvenir de moi :", seSouvenir);
-});
+const mailBtn = document.getElementById("mail-btn");
+if (mailBtn) {
+    mailBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+        
+        const mailRecup = document.getElementById("mail-recup").value;
+        console.log("Mail pour réinitialisation :", mailRecup);
+    });
+}
