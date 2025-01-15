89532430567715dd38c550a0bf70111ef53be1a6
Raphael Vilnet <vilnetr@formation.univ-smb.fr>
2025-01-15 10:38:20 +0100

    ajout multicouleur changement id créneau et ajout calendrier dans vue docteur

--- calendrier/calendrier.js
+++ calendrier/calendrier.js
@@ -30,24 +30,26 @@ var listeJour = ["lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanch
 for (let i = 0 ; i<7 ; i++) {
     var datetemp = new Date();
     datetemp.setDate(date.getDate()+i+1-indice_jour)
-    console.log(datetemp)
 
 
     var div_jour = create("div",main)
     var article = create("article",div_jour)
     var t = create("p",article,listeJour[i]+"\n"+datetemp.toLocaleDateString())
+    var heure = 8;
 
 
 
     for (let j = 0 ; j<72 ; j++) {
         var creneau = create("article",div_jour)
-        creneau.id = i.toString() + j.toString()
+        creneau.id = datetemp.toLocaleDateString() + j.toString()
         creneau.classList.add("creneau")
+        
         if (i == 0 && j%6 == 0) {
             var carre_heure = create("div",creneau)
             carre_heure.classList.add("carre_heure")
-            var heure = (Math.floor(j/6)+8)+"h00"
-            var texte = create("p",carre_heure,heure)
+            
+            var texte = create("p",carre_heure,heure+"h00")
+            heure = heure+1
             texte.classList.add("heure")
         } 
 
@@ -93,20 +95,28 @@ for (let i = 0 ; i<7 ; i++) {
 
 
 
-function create_rdv(horaire_debut,horaire_fin,journee) {
+function create_rdv(horaire_debut,horaire_fin,journee,color="yellow") {
     for (let i = horaire_debut; i<=horaire_fin; i++) {
         console.log(journee.toString()+i.toString())
         let creneau_horaire = document.getElementById(journee.toString()+i.toString())
-        creneau_horaire.style.backgroundColor = "yellow"
+        creneau_horaire.style.backgroundColor = color
 
         if (i%6 == 0 && i != horaire_debut) {
-            creneau_horaire.style.borderTop = "solid 1px yellow"
+            creneau_horaire.style.borderTop = "solid 1px "+color
         }
         
     }
 }
 
-create_rdv(0,71,0)
-create_rdv(31,48,2)
-create_rdv(51,60,2)
-create_rdv(3,6,4)
+
+var lundi = "13/01/2025"
+var mardi = "14/01/2025"
+var mercredi = "15/01/2025"
+var jeudi = "16/01/2025"
+var vendredi = "17/01/2025"
+var samedi = "18/01/2025"
+var dimanche = "19/01/2025"
+create_rdv(0,71,lundi,"blue")
+create_rdv(31,48,jeudi)
+create_rdv(51,60,jeudi,"green")
+create_rdv(6,11,dimanche,"red")
