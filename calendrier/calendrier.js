ab21b4c1d5a8ac5d52214e73eaf81d80c5bce1ae
RBreavy <vilnetraphael@gmail.com>
2025-02-18 15:40:18 +0100

    test

--- calendrier/calendrier.js
+++ calendrier/calendrier.js
@@ -187,7 +187,7 @@ function creation_crenau(indice_div_jour,div_jour,datetemp) {
 }
 
 
-fetch('calendrier/get-data.php')
+fetch('get-data.php')
   .then(response => response.json())
   .then(data => {
     // Utiliser les données
