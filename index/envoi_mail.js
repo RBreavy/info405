7ed2a50c3ad2c866198bd4c43a3b99ead1fe5c65
carotte100 <carottelenullos@gmail.com>
2025-04-01 23:30:54 +0200

    ouch

--- index/envoi_mail.js
+++ index/envoi_mail.js
@@ -3,7 +3,7 @@ function envoiMail(){
         name : "test",
         email : document.getElementById("mail-recup").value ,
         subject : "réinitialiser votre mot de passe",
-        message : "Pour récuperer votre mot de passe cliquer sur ce lien blabla"
+        message : "voici votre code à entrer: 777"
     }
 
     emailjs.send("service_info405","template_1wicn1k", param).then(alert("Mail envoyé !!"));
