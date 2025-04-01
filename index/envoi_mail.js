6b9530e0514d964bfaafcc65df1bbd7948a7e514
carotte100 <carottelenullos@gmail.com>
2025-04-01 23:39:32 +0200

    bug fix

--- index/envoi_mail.js
+++ index/envoi_mail.js
@@ -1,4 +1,6 @@
 function envoiMail(){
+    event.preventDefault();
+
     let param = {
         name : "test",
         email : document.getElementById("mail-recup").value ,
@@ -6,5 +8,12 @@ function envoiMail(){
         message : "voici votre code à entrer: 777"
     }
 
-    emailjs.send("service_info405","template_1wicn1k", param).then(alert("Mail envoyé !!"));
+    emailjs.send("service_info405", "template_1wicn1k", param)
+        .then(function(response) {
+            console.log("Mail envoyé !!", response);
+            alert("Un e-mail a été envoyé à " + param.from_email + " avec les instructions pour réinitialiser votre mot de passe.");
+        }, function(error) {
+            console.log("Erreur lors de l'envoi du mail", error);
+            alert("Une erreur est survenue, veuillez réessayer.");
+        }); 
 }
