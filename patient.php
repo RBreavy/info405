fb1a6e4d09bcc2ad3a31a3e718edda93668e5137
KASSOUMH <hadykassoum2003@gmail.com>
2025-04-01 14:55:47 +0200

    welcome message

--- patient.php
+++ patient.php
@@ -23,8 +23,8 @@ $nom = htmlspecialchars($_SESSION['nom']);
 <body>
     <div class="banner">
         <img src="logo.png" alt="Logo" class="banner-logo">
-        <span class="menu-icon" onclick="openNav()">☰</span> <br>
         <div id="welcome-message" style="margin: 20px; font-size: 18px;">Bienvenue, <?php echo $nom; ?></div>
+        <span class="menu-icon" onclick="openNav()">☰</span> <br>
     </div>
     
     <div id="mySidenav" class="sidenav">
