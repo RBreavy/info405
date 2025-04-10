bc008d56dc5bbd56da1bb6475395ca6fafa1bf4f
carotte100 <carottelenullos@gmail.com>
2025-04-10 22:54:27 +0200

    test mail

--- index/envoi_mail.js
+++ index/envoi_mail.js
@@ -1,19 +1,30 @@
-function envoiMail(){
-    event.preventDefault();
+function envoiMail() {
+    // Récupérer l'email saisi par l'utilisateur
+    const email = document.getElementById('mail-recup').value;
 
-    let param = {
-        name : "test",
-        email : document.getElementById("mail-recup").value ,
-        subject : "réinitialiser votre mot de passe",
-        message : "voici votre code à entrer: 777"
-    }
+    // Générer un lien de réinitialisation (à adapter selon votre logique serveur)
+    const resetLink = `http://51.68.91.213/info2/site/index/reset_mdp.html?token=GENERATED_TOKEN`;
 
-    emailjs.send("service_info405", "template_1wicn1k", param)
+    // Message à inclure dans l'email
+    const message = `Cliquez sur le lien suivant pour réinitialiser votre mot de passe : ${resetLink}`;
+
+    // Paramètres de l'email à envoyer
+    const templateParams = {
+        to_email: email,
+        message: message // Ajoutez ici le message ou toute autre variable nécessaire
+    };
+
+    // Service et template ID à remplacer par les vôtres
+    const serviceID = 'service_18s7vqc';
+    const templateID = 'template_1wicn1k';
+
+    // Envoi de l'email
+    emailjs.send(serviceID, templateID, templateParams)
         .then(function(response) {
-            console.log("Mail envoyé !!", response);
-            alert("Un e-mail a été envoyé à " + param.from_email + " avec les instructions pour réinitialiser votre mot de passe.");
+            console.log('Email envoyé avec succès !', response.status, response.text);
+            alert('Un email de réinitialisation a été envoyé à ' + email);
         }, function(error) {
-            console.log("Erreur lors de l'envoi du mail", error);
-            alert("Une erreur est survenue, veuillez réessayer.");
-        }); 
+            console.log('Erreur lors de l\'envoi de l\'email :', error);
+            alert('Une erreur est survenue, veuillez réessayer plus tard.');
+        });
 }
