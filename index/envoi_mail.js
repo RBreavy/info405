221097c94398c4e528b05091eeb68a335bdc9a10
carotte100 <carottelenullos@gmail.com>
2025-04-11 22:36:11 +0200

    re test

--- index/envoi_mail.js
+++ index/envoi_mail.js
@@ -10,32 +10,36 @@ function generateToken(length = 32) {
 
 
 function envoiMail() {
-    // Récupérer l'email saisi par l'utilisateur
     const email = document.getElementById('mail-recup').value;
-
     const token = generateToken();
     const resetLink = `http://51.68.91.213/info2/site/index/reset_mdp.html?token=${token}`;
 
     const message = `Cliquez sur le lien suivant pour réinitialiser votre mot de passe : ${resetLink}`;
 
-    // Paramètres de l'email à envoyer
-    const templateParams = {
-        to_email: email,
-        message: message,
-        subject: "réinitialiser votre mot de passe"
-    };
+    fetch('/info2/PHP/set_token.php', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email: email, token: token })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            const templateParams = {
+                to_email: email,
+                message: message,
+                subject: "Réinitialiser votre mot de passe"
+            };
 
-    // Service et template ID
-    const serviceID = 'service_info405';
-    const templateID = 'template_1wicn1k';
-
-    // Envoi de l'email
-    emailjs.send(serviceID, templateID, templateParams)
-        .then(function(response) {
-            console.log('Email envoyé avec succès !', response.status, response.text);
-            alert('Un email de réinitialisation a été envoyé à ' + email);
-        }, function(error) {
-            console.log('Erreur lors de l\'envoi de l\'email :', error);
-            alert('Une erreur est survenue, veuillez réessayer plus tard.');
-        });
+            emailjs.send('service_info405', 'template_1wicn1k', templateParams)
+                .then(function(response) {
+                    console.log('Email envoyé avec succès !', response.status, response.text);
+                    alert('Un email de réinitialisation a été envoyé à ' + email);
+                }, function(error) {
+                    console.log('Erreur lors de l\'envoi de l\'email :', error);
+                    alert('Une erreur est survenue, veuillez réessayer plus tard.');
+                });
+        } else {
+            alert('Erreur lors de l’enregistrement du token.');
+        }
+    });
 }
