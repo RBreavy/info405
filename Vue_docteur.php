06c0545fa4476c5fc4824e36df9bc6265260c2f5
carotte100 <carottelenullos@gmail.com>
2025-04-12 23:20:35 +0200

    css #1

--- Vue_docteur.php
+++ Vue_docteur.php
@@ -20,7 +20,7 @@ $nom = htmlspecialchars($_SESSION['nom']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style_utilisateur.css">
+    <link rel="stylesheet" href="/info2/site/CSS/style_utilisateur.css">
     <link rel="stylesheet" href="calendrier/calendrier.css">
     <title>Docteur page</title>
 
