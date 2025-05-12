51f27f5fc2eadfe8b3d8267b92dab2b6214ff836
carotte100 <carottelenullos@gmail.com>
2025-05-12 08:37:54 +0200

    bug fix

--- calendrier/calendrier.js
+++ calendrier/calendrier.js
@@ -72,7 +72,7 @@ async function chargerEtAfficherRDV() {
                     const h_debut = (debut.getHours() - 8) * 6 + Math.floor(debut.getMinutes() / 10);
                     const h_fin = (fin.getHours() - 8) * 6 + Math.floor(fin.getMinutes() / 10) - 1;
 
-                    if (await estMedecin(nomUtilisateur)) {
+                    if (estMedecin(nomUtilisateur)) {
                         create_rdv(h_debut, h_fin, jourStr, jourStr, couleur, nom);
                     } else {
                         create_rdv(h_debut, h_fin, jourStr, jourStr, "black", nom);
