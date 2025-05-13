3592b45564506b3a8ead5181e7ed0dbfc3a486d5
RBreavy <vilnetraphael@gmail.com>
2025-05-13 14:55:08 +0200

    ajout indisp temp

--- JS/calendrier.js
+++ JS/calendrier.js
@@ -19,7 +19,7 @@ const dateInput = document.getElementById("calendrier");
 dateInput.addEventListener('change', () => {
     
     [year, month, day] = dateInput.value.split("/").map(Number);
-    console.log(year,month,day);
+    console.log(dateInput.value);
     if (year >= 2000 && year <= 2100) {
         console.log("Date sélectionnée:", dateInput.value);
         dateInput.value = "";
