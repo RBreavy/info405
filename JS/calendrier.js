d49ee45bc5a1374abe11bcecddb10d4ea5140a55
carotte100 <carottelenullos@gmail.com>
2025-05-14 10:33:52 +0200

    revert

--- JS/calendrier.js
+++ JS/calendrier.js
@@ -334,7 +334,7 @@ async function create_rdv(horaire_debut, horaire_fin, journee, journee_fin = jou
             const box = create("article", premierCreneau);
             box.classList.add("rdv");
             
-            const toggleButton = create("div", box, " ");
+            const toggleButton = create("div", box, "Détails");
             toggleButton.classList.add("toggle_button");
             toggleButton.style.fontSize = "0.5rem";
             
