38682fc25da724f7e325dda4a12635d7f8c03afa
carotte100 <carottelenullos@gmail.com>
2025-05-14 08:41:40 +0200

    supprm vilain comm

--- JS/calendrier.js
+++ JS/calendrier.js
@@ -297,35 +297,29 @@ function conversion_heure_en_id(heure_debut) {
 
 async function create_rdv(horaire_debut, horaire_fin, journee, journee_fin = journee, color, nom, estDoc = false) {
     if (horaire_debut > -1 && horaire_fin < 72 && document.getElementById(journee)) {
-        // Apply to all cells in appointment
         for (let i = horaire_debut; i <= horaire_fin; i++) {
             const creneau = document.getElementById(journee + i);
             if (!creneau) continue;
             
-            // Apply background color
             creneau.style.setProperty('--border-color', color);
             creneau.classList.add("custom_bg_color");
             
-            // Reset any previous styles
             creneau.style.boxShadow = "none";
             
-            // Style inner cells (no visible borders)
             if (i > horaire_debut && i < horaire_fin) {
                 creneau.style.borderTop = "0px solid transparent";
                 creneau.style.borderBottom = "0px solid transparent";
             }
             
-            // Style first cell - handle top edge case
             if (i === horaire_debut) {
-                if (i > 0) { // Not at the very top
+                if (i > 0) {
                     creneau.style.boxShadow = "0px -1px 0px 0px black";
-                } // Leave original border if at the top
+                }
                 creneau.style.borderBottom = "0px solid transparent";
                 creneau.style.position = "relative";
                 creneau.style.zIndex = "1";
             }
             
-            // Style last cell
             if (i === horaire_fin) {
                 creneau.style.boxShadow = "0px 1px 0px 0px black";
                 creneau.style.borderTop = "0px solid transparent";
@@ -334,7 +328,6 @@ async function create_rdv(horaire_debut, horaire_fin, journee, journee_fin = jou
             }
         }
         
-        // Rest of the code remains the same
         const premierCreneau = document.getElementById(journee + horaire_debut);
         if (premierCreneau && !premierCreneau.querySelector(".rdv")) {
             const box = create("article", premierCreneau);
