475e91c65ca4d346fbf2feb01db13fcd015da169
carotte100 <carottelenullos@gmail.com>
2025-05-14 10:32:01 +0200

    fix

--- JS/calendrier.js
+++ JS/calendrier.js
@@ -295,7 +295,7 @@ function conversion_heure_en_id(heure_debut) {
     return (parseInt(heure_debut.slice(0, 2)) - 8) * 6 + parseInt(heure_debut.slice(3, 4));
 }
 
-async function create_rdv(horaire_debut, horaire_fin, journee, journee_fin = journee, color, nom="", estDoc = false) {
+async function create_rdv(horaire_debut, horaire_fin, journee, journee_fin = journee, color, nom, estDoc = false) {
     if (horaire_debut > -1 && horaire_fin < 72 && document.getElementById(journee)) {
         for (let i = horaire_debut; i <= horaire_fin; i++) {
             const creneau = document.getElementById(journee + i);
@@ -334,10 +334,9 @@ async function create_rdv(horaire_debut, horaire_fin, journee, journee_fin = jou
             const box = create("article", premierCreneau);
             box.classList.add("rdv");
             
-            // const toggleButton = create("div", box, "Détails");
-            // toggleButton.style.fontSize = "2%";
-            // toggleButton.classList.add("toggle_button");
-            // toggleButton.style.fontSize = "0.8rem";
+            const toggleButton = create("div", box, "Détails");
+            toggleButton.classList.add("toggle_button");
+            toggleButton.style.fontSize = "0.5rem";
             
             const details = create("div", box);
             details.classList.add("rdv_details");
@@ -350,8 +349,6 @@ async function create_rdv(horaire_debut, horaire_fin, journee, journee_fin = jou
             create("p", details, calcul_duree(horaire_debut, horaire_fin - horaire_debut + 1));
             if (estDoc) {
                 create("p", details, `Nom : ${nom}`);
-            } else {
-                create("p", details, `Nom médecin : ${nom}`);
             }
             
             creneau.addEventListener("click", () => {
